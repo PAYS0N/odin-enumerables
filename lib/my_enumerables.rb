ae4e3cdcb5d128ca5d3b27ca7432cae4f2c97704
Payson Eberz <63822744+PAYS0N@users.noreply.github.com>
2024-11-11 13:18:03 -0600

    Complete project for arrays

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,4 @@
 module Enumerable
-  # Your code goes here
 end
 
 # You will first have to define my_each
@@ -8,4 +7,74 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    0.upto(self.length-1) do |i|
+      yield(self[i])
+    end
+    self
+  end
+  # Your code goes here
+  def my_each_with_index
+    0.upto(self.length-1) do |i|
+      yield(self[i], i)
+    end
+    self
+  end
+
+  def my_select
+    newArr = []
+    self.my_each do |unit|
+      newArr.push(unit) if yield(unit)
+    end
+    newArr
+  end
+
+  def my_all?
+    self.my_each do |unit|
+      return false unless yield(unit)
+    end
+    true
+  end
+
+  def my_any?
+    self.my_each do |unit|
+      return true if yield(unit)
+    end
+    false
+  end
+
+  def my_none?
+    self.my_each do |unit|
+      return false if yield(unit)
+    end
+    true
+  end
+
+  def my_count
+    count = 0
+    if block_given?
+      self.my_each do |unit|
+        count += 1 if yield(unit)
+      end
+      count
+    else
+      self.length
+    end
+  end
+
+  def my_map
+    newArr = []
+    self.my_each do |unit|
+      newArr << yield(unit)
+    end
+    newArr
+  end
+
+  def my_inject(val = nil)
+    accumulator = val == nil ? self[0] : val
+    self.my_each do |unit|
+      accumulator = yield(accumulator, unit)
+    end
+    accumulator
+  end
 end
